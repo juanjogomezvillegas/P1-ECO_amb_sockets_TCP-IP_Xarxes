c213fa394960926fdedb4cbe6de50fc32e2616f7
juanjogomezvillegas <juanjogomvil@gmail.com>
2025-10-13 15:03:44 +0200

    canvis

--- p1-cliECO.c
+++ p1-cliECO.c
@@ -13,6 +13,7 @@
 /* Inclusió de llibreries, p.e. #include <stdio.h> o #include "meu.h"     */
 
 #include "p1-tTCP.h"
+#include <stdbool.h>
 
 /* Definició de constants, p.e.,                                          */
 
@@ -23,13 +24,28 @@
 /* fer-les conegudes des d'aquí fins al final d'aquest fitxer, p.e.,      */
 
 /* int FuncioInterna(arg1, arg2...);                                      */
+bool strIsEqual(char s1[], char s2[]);
+void AskIpAddr(char* ip);
+void AskPort(int* port);
 
 int main(int argc,char *argv[])
 {
- /* Declaració de variables, p.e., int n;                                 */
+    /* Declaració de variables, p.e., int n;                                 */
+    int scon, i;
+    int bytes_llegits, bytes_escrits;
+    char buffer[200];
+    char iprem[16];
+    int portrem;
 
- /* Expressions, estructures de control, crides a funcions, etc.          */
+    /* Expressions, estructures de control, crides a funcions, etc.          */
+    AskIpAddr(iprem);
+    AskPort(&portrem);
 
+    if ((scon = TCP_CreaSockClient(iprem, portrem)) == -1) {
+        exit(-1);
+    }
+
+    exit(0);
 }
 
 /* Definició de funcions INTERNES, és a dir, d'aquelles que es faran      */
@@ -41,3 +57,16 @@ int main(int argc,char *argv[])
 {
 	
 } */
+bool strIsEqual(char s1[], char s2[]) {
+	return strcmp(s1, s2) == 0;
+}
+
+void AskIpAddr(char* ip) {
+    printf("Entra l'@IP remota:\n");
+    scanf("%15s", ip);
+}
+
+void AskPort(int* port) {
+    printf("Entra el port remot:\n");
+    scanf("%d", port);
+}
--- p1-serECO.c
+++ p1-serECO.c
@@ -13,6 +13,7 @@
 /* Inclusió de llibreries, p.e. #include <stdio.h> o #include "meu.h"     */
 
 #include "p1-tTCP.h"
+#include <stdbool.h>
 
 /* Definició de constants, p.e.,                                          */
 
@@ -23,13 +24,53 @@
 /* fer-les conegudes des d'aquí fins al final d'aquest fitxer, p.e.,      */
 
 /* int FuncioInterna(arg1, arg2...);                                      */
+bool strIsEqual(char s1[], char s2[]);
 
 int main(int argc,char *argv[])
 {
- /* Declaració de variables, p.e., int n;                                 */
+    /* Declaració de variables, p.e., int n;                                 */
+    int sesc, scon, i;
+    int bytes_llegits, bytes_escrits;
+    char buffer[200];
+    char iploc[16] = "192.168.0.17";
+    int portloc = 3000;
 
- /* Expressions, estructures de control, crides a funcions, etc.          */
+    /* Expressions, estructures de control, crides a funcions, etc.          */
+    if ((sesc = TCP_CreaSockServidor(iploc, portloc)) == -1) {
+        exit(-1);
+    }
 
+    for (;;) {
+        if ((scon = TCP_AcceptaConnexio(sesc, iploc, portloc)) == -1) {
+            exit(-1);
+        }
+
+        while (!(strIsEqual(buffer, "FI\n"))) {
+            if ((bytes_llegits=TCP_Rep(scon, buffer, 200)) == -1) {
+                perror("Error en read");
+                exit(-1);
+            }
+
+            buffer[bytes_llegits] = '\0'; // inserció del caràcter null al buffer per poder comparar-lo
+            
+            if (!(strIsEqual(buffer, "FI\n"))) {
+                if ((bytes_escrits = write(1, buffer, bytes_llegits)) == -1) {
+                    perror("Error en write");
+                    exit(-1);
+                }
+                if ((bytes_escrits = TCP_Envia(scon, buffer, bytes_llegits)) == -1) {
+                    perror("Error en write ECO");
+                    exit(-1);
+                }                
+            }
+        }
+    }
+
+    if (TCP_TancaSock(sesc) == -1) {
+        exit(-1);
+    }
+
+    exit(0);
 }
 
 /* Definició de funcions INTERNES, és a dir, d'aquelles que es faran      */
@@ -41,3 +82,6 @@ int main(int argc,char *argv[])
 {
 	
 } */
+bool strIsEqual(char s1[], char s2[]) {
+	return strcmp(s1, s2) == 0;
+}
